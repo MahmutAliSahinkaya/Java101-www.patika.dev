1644a7ef657db0eb1d7e249a428d2c7180b10cdf
Mahmut Ali Şahinkaya <107810135+MahmutAliSahinkaya@users.noreply.github.com>
2022-10-04 18:05:29 +0300

    Add files via upload

---
+++ TaksimetreHesaplayanProgram/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total, startPrice =10;
+
+        Scanner input = new  Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz : ");
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = (total <20) ? 20:total;
+        System.out.println("Toplam Tutar : " +total);
+
+
+
+    }
+}
